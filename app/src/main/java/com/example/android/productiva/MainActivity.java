096688c531976a2f0c9d7c74d0af90aabee47622
tausif0235 <tausifahmed0235@gmail.com>
2021-01-12 03:26:29 +0600

    welcomePage added

---
+++ app/src/main/java/com/example/android/productiva/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.android.productiva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //variables
+    Animation topAnim, bottomAnim, titleAnim;
+    ImageView logo;
+    TextView title, tagline;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+        titleAnim = AnimationUtils.loadAnimation(this,R.anim.title_anim);
+
+        //Hooks
+        logo = findViewById(R.id.imageView);
+        title = findViewById(R.id.textView2);
+        tagline = findViewById(R.id.textView3);
+
+        logo.setAnimation(topAnim);
+        title.setAnimation(titleAnim);
+        tagline.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+               // finish();
+
+            }
+        },  SPLASH_SCREEN);
+    }
+}
